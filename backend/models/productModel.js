1348c84cfdbc62ee1cafdb2bc09085d831055a4e
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2023-09-07 12:03:06 +0530

    product schema added

---
+++ backend/models/productModel.js
@@ -0,0 +1,37 @@
+import mongoose, { Schema } from "mongoose";
+
+const productSchema = Schema({
+    user:{type:mongoose.Schema.Types.ObjectId,
+    require: true,
+    ref:"User",
+    },
+    name:{type:String, require:true},
+    image:{type:String, require:true},
+    brand:{type:String, require:true},
+    category:{type:String, require:true},
+    description:{type:String, require:true},
+    reviews:[reviewSchema],
+    rating:{type:Number, require: true, default:0},
+    numReviews:{type:Number, require:true, default:0},
+    price:{type:Number, require:true, default:0},
+    countInStock:{type:Number, require:true, default:0},
+},{
+    timestamps:true,
+})
+
+const reviewSchema =mongoose.Schema({
+    name:{type:String, require:true},
+    rating:{type: Number, require:true},
+    comment: {type: String, require:true},
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        require: true,
+        ref: "User",
+    },
+},{
+    timestamps:true,
+}
+);
+
+const Product = mongoose.model("Product",productSchema);
+export default Product;
