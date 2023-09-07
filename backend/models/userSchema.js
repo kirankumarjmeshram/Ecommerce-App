3465d7f7049e33159e1665bf15458dac07043d7d
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2023-09-07 18:11:12 +0530

    Order Schema added

--- backend/models/userSchema.js
+++ backend/models/userSchema.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 import bcrypt from 'bcryptjs';
-const userSchema = new mongoose.Schema({
+const userSchema = mongoose.Schema({
     name:{type:String, require: true},
     email:{type:String, require:true, unique:true},
     password:{type:String, require:true},
