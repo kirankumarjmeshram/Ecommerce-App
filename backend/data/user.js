024970d287d80484a62c835710297701ca736b3b
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2023-09-11 17:43:26 +0530

    Create user.js

---
+++ backend/data/user.js
@@ -0,0 +1,21 @@
+import bcrypt from 'bcryptjs';
+const user = [
+    {
+        name: `${process.env.NAME}`,
+        email: `${process.env.EMAIL}`,
+        password: bcrypt.hashSync(`${process.eventNames.PASS}`,10),
+        isAdmin:true,
+    },
+    {
+        name: 'Test 1',
+        email: 'test1@email.com',
+        password: bcrypt.hashSync('123456', 10),
+      },
+      {
+        name: 'Test 2',
+        email: 'test2@email.com',
+        password: bcrypt.hashSync('123456', 10),
+      },
+]
+
+export default users;
