cf1bd54def99169c5618c9409912eef76c12fcd3
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2023-09-12 10:42:26 +0530

    added routes for products

--- backend/server.js
+++ backend/server.js
@@ -2,25 +2,17 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './config/db.js';
-import products from './data/products.js'
-const port = process.env.PORT || 5000;
+import producRoutes from './routes/productRoutes.js';
+const port = process.env.PORT || 8000;
 
-const app = express();
 connectDB();
+const app = express();
 
 app.get('/',(req, res)=>{
     res.send('Api is running');
 });
 
-app.get('/api/products', (req, res)=>{
-    res.json(products)
-});
-
-app.get('/api/products/:id', (req, res)=>{
-    const product = products.find((p)=> p._id === req.params.id);
-    res.json(product);
-   
-})
+app.use('/api/products', producRoutes);
 
 app.listen(port, ()=>{
     console.log(`server is running on port ${port}`)
