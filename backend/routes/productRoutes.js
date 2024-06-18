f94050ff24ec5ed73974f44451e54ffa65b9a731
Kirankumar J Meshram <kirankumar21895@gmail.com>
2024-06-18 11:32:04 +0530

    updated

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,23 +1,24 @@
 import express from 'express';
 const router = express.Router();
-import asyncHandler from '../middleware/asyncHandler.js';
-import Product from '../models/productModel.js'
+import { getProducts, getProductById } from '../controllers/productController.js';
 
+// router.get('/', asyncHandler(async (req, res)=>{
+//     const products = await Product.find({}); // passing empty object to get all
+//     res.json(products);
+// }));
 
-router.get('/', asyncHandler(async (req, res)=>{
-    const products = await Product.find({}); // passing empty object to get all
-    res.json(products);
-}));
+// router.get('/:id', asyncHandler(async(req, res)=>{
+//     const product = await Product.findById(req.params.id);
+//     if(product){
+//         return res.json(product);
+//     } else {
+//         // res.status(404).json('Product not found' );// shows html page
+//         res.status(404);
+//         throw new Error('Resource not found');// no more html page 
+//     }
+// }));
 
-router.get('/:id', asyncHandler(async(req, res)=>{
-    const product = await Product.findById(req.params.id);
-    if(product){
-        return res.json(product);
-    } else {
-        // res.status(404).json('Product not found' );// shows html page
-        res.status(404);
-        throw new Error('Resource not found');// no more html page 
-    }
-}));
+router.route('/').get(getProducts);
+router.route('/:id').get(getProductById);
 
 export default router;
