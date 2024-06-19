dfc395137a398f3c4977149e062a78038f9b95f6
Kirankumar J Meshram <kirankumar21895@gmail.com>
2024-06-19 13:59:07 +0530

    Update productsApiSlice.js

--- frontend/src/slices/productsApiSlice.js
+++ frontend/src/slices/productsApiSlice.js
@@ -5,11 +5,20 @@ export const productsApiSlice = apiSlice.injectEndpoints({
     endpoints:(builder) => ({ 
         getProducts: builder.query({
             query: () => ({
-                url: PRODUCT_URL
+                url: `/${PRODUCT_URL}`
             }),
+            keepUnusedDataFor: 5,
         }),
-        keepUnusedDataFor: 5
+        
+        getProductDetail: builder.query({
+            query: (productId) => ({
+                url: `/${PRODUCT_URL}/${productId}`
+                // url: `/api/products/${productId}`,
+            }),
+            keepUnusedDataFor: 5
+        }),
+        
     })
 });
 
-export const { useGetProductsQuery } = productsApiSlice;
+export const { useGetProductsQuery, useGetProductDetailQuery } = productsApiSlice;
