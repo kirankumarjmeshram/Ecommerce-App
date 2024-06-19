dfb765c525f7f37de7829a5f4bdb462d6aa14213
Kirankumar J Meshram <kirankumar21895@gmail.com>
2024-06-19 14:04:13 +0530

    implemented Redux successfully

---
+++ frontend/src/constants.js
@@ -0,0 +1,6 @@
+// export const BASE_URL = process.env.NODE_ENV === 'developement'? 'http://localhost:5001': '';
+export const BASE_URL ='';
+export const PRODUCT_URL = 'api/products';
+export const USERS_URL = 'api/users';
+export const ORDERS_URL = 'api/orders';
+export const RAZORPAY_URL = 'api/razorpay';
--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,11 +1,12 @@
 // import { useEffect, useState } from 'react';
 // import axios from 'axios'
-import {Row, Col} from 'react-bootstrap';
-import Product from '../components/Product';
-import { useGetProductsQuery } from '../slices/productsApiSlice';
+import { Row, Col } from "react-bootstrap";
+import Product from "../components/Product";
+import { useGetProductsQuery } from "../slices/productsApiSlice";
+import Loader from "../components/Loader";
 
 const HomeScreen = () => {
- // const [products, setProducts] = useState([]);
+  // const [products, setProducts] = useState([]);
 
   // useEffect(()=>{
   //   const fetchProducts = async ()=>{
@@ -16,25 +17,28 @@ const HomeScreen = () => {
   //   fetchProducts();
   // },[])
 
-  const {data:products, isLoading, error} = useGetProductsQuery();
+  const { data: products, isLoading, error } = useGetProductsQuery();
 
   return (
     <>
-        {isLoading ?(
-          <h2>Loading...</h2>
-        ):error?(<div>{error?.data?.message || error.error}</div>):(<>
-            <h1>Latest Products</h1>
-            <Row>
-                {products.map((product)=>(
-                    <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                        <Product product={product}/>
-                    </Col>
-                ))}
-            </Row>
-          </>)}
-       
+      {isLoading ? (
+        <Loader/>
+      ) : error ? (
+        <div>{error?.data?.message || error.error}</div>
+      ) : (
+        <>
+          <h1>Latest Products</h1>
+          <Row>
+            {products.map((product) => (
+              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                <Product product={product} />
+              </Col>
+            ))}
+          </Row>
+        </>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
