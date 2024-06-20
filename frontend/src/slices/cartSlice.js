c72b442ad384c79535605fa5220cf410d4228145
Kirankumar J Meshram <kirankumar21895@gmail.com>
2024-06-20 16:44:03 +0530

    updated spelling mistake

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -1,6 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit"; // we dont need createApi here
 
-const initialState = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : {cartItems: []};
+const initialState = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : { cartItems: [] };
 //  {cartItems: [], totalQuantity: 0, totalAmount: 0};
 
 const addDecimals = (num) => {
@@ -11,28 +11,28 @@ const cartSlice = createSlice({
     name: 'cart',
     initialState,
     reducers: {
-        addToCard: (state, action) => {
+        addToCart: (state, action) => {
             const item = action.payload;
             const existItem = state.cartItems.find((x) => x._id === item.__id);
 
-            if(existItem) {
+            if (existItem) {
                 state.cartItems = state.cartItems.map((x) => x._id === existItem._id ? item : x)
-            }else{
+            } else {
                 state.cartItems = [...state.cartItems, item];
             }
             //calculate the item price
             state.itemPrice = addDecimals(state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0));
-            
+
             //calculate shipping price (if order is over 1000 then free, else 100 shipping )
             state.shippingPrice = addDecimals(state.itemPrice > 1000 ? 0 : 100);
-            
+
             //calculate tax price (lets say 12%)
             state.taxPrice = addDecimals(Number((0.12 * state.itemPrice).toFixed(2)));
 
             //calculate total price
             state.totalPrice = (
-                Number(state.itemPrice)+
-                Number(state.shippingPrice)+
+                Number(state.itemPrice) +
+                Number(state.shippingPrice) +
                 Number(state.taxPrice)
             ).toFixed(2);
 
@@ -41,5 +41,5 @@ const cartSlice = createSlice({
     },
 })
 
-export const {addToCart} = cartSlice.actions;
+export const { addToCart } = cartSlice.actions;
 export default cartSlice.reducer;
