aa22c128932a7e92e7a6a5661b04fec5a73baf3b
Kirankumar J Meshram <kirankumar21895@gmail.com>
2024-06-25 16:37:06 +0530

    .

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -13,9 +13,9 @@ import {
     updateUser
 } from '../controllers/userController.js';
 
-router.route('/').get(getUsers);
+router.route('/').post(registerUser).get(getUsers);
 router.post('/logout',logoutUser);
-router.post('/login',logoutUser);
+router.post('/login',authUser);
 router.route('/user').get(getUserProfile).post(updateUserProfile);//in /profile if get request then get user profile else if it post request then update user profile
 router.route('/:id').delete(deleteUser).get(getUserById).put(updateUser);
 
--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ dotenv.config();
 // console.log('Loaded environment variables:', process.env.PORT);
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 const port = process.env.PORT || 5001;
 
@@ -18,6 +19,7 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes)
 
 app.use(notFound);
 app.use(errorHandler);
