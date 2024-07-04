6b74cd0e628d2a09464b221764331ba7ddd29261
Kirankumar J Meshram <kirankumar21895@gmail.com>
2024-07-04 17:36:04 +0530

    Create and get orders

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -5,22 +5,64 @@ import Order from "../models/orderModel.js";
 // @route POST / api / orders
 // @acces Private
 const addOrderItems = asyncHandler(async (req, res) =>{
-    res.send('add order item');
+    // res.send('add order item');
+    const {
+        orderItems,
+        shippingAddress,
+        paymentMethod,
+        itemsPrice,
+        taxPrice,
+        shippingPrice,
+        totalPrice,
+        } = req.body;
+    if(orderItems && orderItems.length === 0) {
+        res.send(400);
+        throw new Error("No order Items")
+    } else {
+        const order = new Order({
+            orderItems: orderItems.map((x)=>({
+                ...x,
+                product: x_id,
+                _id:undefined
+            })),
+            user: req.user._id,
+            shippingAddress,
+            paymentMethod,
+            itemsPrice,
+            taxPrice,
+            shippingPrice,
+            totalPrice,
+        });
+        const createOrder =  await order.save();
+        res.status(201).json(createOrder);
+    }
 });
 
 // @desc Get logged in user orders
 // @route GET /api /orders/myorders
 // @acces Private
 const getMyOrders = asyncHandler(async (req, res) =>{
-    res.send('get My Orders');
+    // res.send('get My Orders');
+    const orders = await Order.find({user: req.user._id});
+    res.status(200).json(orders);
 });
 
 // @desc Get order by ID
 // @route GET/ api / orders/:id
 // @acces Private // admin
 const getOrderById = asyncHandler(async (req, res) =>{
-    res.send('get order by id');
-});
+    // res.send('get order by id');
+    const order = await Order.findOrderById(req.params.id);
+}).populate(
+    'user',
+    'name email');
+    if(order) {
+        res.status(200).json(order);
+    } else {
+        res.status(404);
+        throw new Error('Order not found');
+        
+    }
 
 // @desc Update order to paid
 // @route GET / api / orders/:id/pay
