bc265ed660e8ad443e283a4c4643032bc2bb1b1b
Kirankumar J Meshram <kirankumar21895@gmail.com>
2024-07-06 11:18:40 +0530

    correction in orderModel change orderItem to orderItems

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const orderSchema = mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" },
-  orderItem: [
+  orderItems: [
     {
       name: { type: String, require: true },
       qty: { type: Number, required: true },
