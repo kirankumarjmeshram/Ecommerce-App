732804bbd9784f3bdc2e4ca2c79f104df8129257
Kirankumar J Meshram <kirankumar21895@gmail.com>
2024-07-06 12:13:38 +0530

    corrected link

--- frontend/src/screens/PlaceOrderScreen.jsx
+++ frontend/src/screens/PlaceOrderScreen.jsx
@@ -82,7 +82,7 @@ const PlaceOrderScreen = () => {
                           />
                         </Col>
                         <Col>
-                          <Link to={`/product/${item.product}`}>
+                          <Link to={`/product/${item._id}`}>
                             {item.name}
                           </Link>
                         </Col>
